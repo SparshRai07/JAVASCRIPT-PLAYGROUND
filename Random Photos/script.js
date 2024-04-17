d98e7c74642ad018f626075e218c0384f9234dfb
Sparsh Rai <cakelllkami@gmail.com>
2024-04-17 18:24:21 +0545

    Random Photos Generator

---
+++ Random Photos/script.js
@@ -0,0 +1,20 @@
+const imageContainerEl = document.querySelector(".image-container")
+const btnEl = document.querySelector(".btn")
+
+let imageNum = 6;
+
+btnEl.addEventListener("click", () => {
+   imageNum += 6;
+   addNewImages();
+});
+
+function addNewImages() {
+   for (let index = 0; index < imageNum; index ++)
+   {
+      const newImgEl = document.createElement("img");
+      newImgEl.src=`https://picsum.photos/200/300?random=${Math.floor(Math.random()*2000)}`;
+      imageContainerEl.appendChild(newImgEl);
+
+}
+};
+
