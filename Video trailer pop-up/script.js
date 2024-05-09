6b83b6151fcfc9e4b232a48d5f79a327f8540d4d
Sparsh Rai <cakelllkami@gmail.com>
2024-05-09 16:40:20 +0545

    video pop up

---
+++ Video trailer pop-up/script.js
@@ -0,0 +1,17 @@
+const btnEl = document.querySelector(".btn")
+const closeIconEl = document.querySelector(".close-icon")
+const trailerContainerEl = document.querySelector(".trailer-container");
+
+btnEl.addEventListener("click", () =>{
+   trailerContainerEl.classList.remove("active");
+})
+
+closeIconEl.addEventListener("click", ()=> {
+   trailerContainerEl.classList.add("active");
+});
+
+closeIconEl.addEventListener("click", ()=> {
+   trailerContainerEl.classList.add("active");
+   VideoColorSpace.pause();
+   VideoColorSpace.currentTime = 0;
+});
