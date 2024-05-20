ed8d71ce9033ece9d0895deca45047cd343e4fb4
Sparsh Rai <cakelllkami@gmail.com>
2024-05-20 19:44:47 +0545

    FUNDAMENTALS

---
+++ Guess My Number/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// document.querySelector('.message').textContent;
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+
